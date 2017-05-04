8dd3b37bbd9bfa8e1eb2219223cc7ba0da3896a3
einavf <einav.friedman@kenshoo.com>
2017-05-04 11:56:51 +0300

    change name of job

--- Bind-DNS-dsl.groovy
+++ Bind-DNS-dsl.groovy
@@ -1,20 +1,20 @@
-freeStyleJob("Bind-DNS-dsl-test1"){
-    	description 'dsl for the Bind-DNS job'
-  		label('master')
-  		keepDependencies(true)
+freeStyleJob("Bind-DNS-dsl"){
+      description 'dsl for the Bind-DNS job'
+      label('master')
+      keepDependencies(true)
 
     logRotator(20,40)
 
     scm {
         git {
             remote {
-      //          url('git@github.com:kenshoo/jenkins.git')
-      //          credentials('3f510033-65a9-4afd-9851-c7359bd3f9db')
-      //          refspec('+refs/pull/*:refs/remotes/origin/pr/*')
+                url('git@github.com:kenshoo/jenkins.git')
+                credentials('3f510033-65a9-4afd-9851-c7359bd3f9db')
+                refspec('+refs/pull/*:refs/remotes/origin/pr/*')
             }
 
             configure { node ->
-      //          node / 'extensions' / 'hudson.plugins.git.extensions.impl.CleanBeforeCheckout' {}
+                node / 'extensions' / 'hudson.plugins.git.extensions.impl.CleanBeforeCheckout' {}
             }
 
             branch("origin/master")
@@ -23,23 +23,29 @@ freeStyleJob("Bind-DNS-dsl-test1"){
 
 
     configure { project ->
-      	def builder = project / 'builders'
-      		builder << {
+        def builder = project / 'builders'
+          builder << {
             'hudson.tasks.Shell' {
-                    command "cd python_scripts\n" +
-                            ". init.sh\n" +
-                            "nosetests\n"
+                    command "#!/bin/bash\n" +
+              "sudo named-checkzone nyc3.example.com example.com.txt\n" +                
+              "if [ '\$'? -ne 0 ];\n" +                
+              " then\n" +                
+                "echo ************************************************************************************\n" +             
+              "echo named-checkzone failed due to errors in the file, please fix and run the job again\n" +
+                "echo *************************************************************************************\n" +
+                 "exit 1\n" +
+              "fi"
             }
           }
 
         project / 'properties' <<'com.coravy.hudson.plugins.github.GithubProjectProperty'(plugin:'github@1.11.3'){
-        //      projectUrl 'https://github.com/kenshoo/jenkins/'
+              projectUrl 'https://github.com/kenshoo/jenkins/'
         }
     }
 
     configure { project ->
-        	def publishers = project / 'publishers'
-        		publishers << {
+          def publishers = project / 'publishers'
+            publishers << {
 
               'hudson.tasks.Mailer'{
                   recipients "\${ghprbActualCommitAuthorEmail}"
