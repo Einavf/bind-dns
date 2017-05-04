c964e02fc1b560b2e5d4b4cdf412256c04ce63b8
einavf <einav.friedman@kenshoo.com>
2017-05-04 12:32:52 +0300

    add jobs folder

--- jobs/Bind_DNS_dsl.groovy
+++ jobs/Bind_DNS_dsl.groovy
@@ -8,9 +8,8 @@ freeStyleJob("Bind-DNS-test1"){
     scm {
         git {
             remote {
-                url('git@github.com:kenshoo/jenkins.git')
-                credentials('3f510033-65a9-4afd-9851-c7359bd3f9db')
-                refspec('+refs/pull/*:refs/remotes/origin/pr/*')
+                url('https://github.com/Einavf/bind-dns.git')
+                
             }
 
             configure { node ->
@@ -39,7 +38,7 @@ freeStyleJob("Bind-DNS-test1"){
           }
 
         project / 'properties' <<'com.coravy.hudson.plugins.github.GithubProjectProperty'(plugin:'github@1.11.3'){
-              projectUrl 'https://github.com/kenshoo/jenkins/'
+              projectUrl 'https://github.com/Einavf/bind-dns.git'
         }
     }
 
