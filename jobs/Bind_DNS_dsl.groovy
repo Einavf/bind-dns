805273e3c94b1776c16c143a67e54f4f61a3f0fb
einavf <einav.friedman@kenshoo.com>
2017-05-06 09:49:00 +0300

    add python file

--- jobs/Bind_DNS_dsl.groovy
+++ jobs/Bind_DNS_dsl.groovy
@@ -21,25 +21,22 @@ freeStyleJob("Bind-DNS-test1"){
     }
 
 
-     configure { project ->
+    configure { project ->
         def builder = project / 'builders'
           builder << {
             'hudson.tasks.Shell' {
                     command "#!/bin/bash\n" +
               "sudo named-checkzone nyc3.example.com example.com.txt\n" +                
-              "if [ '\$'? -ne 0 ];\n" +                
+              'if [ \$? -ne 0 ];\n' +                
               "then\n" +                
-              'echo \n' +             
-              'echo \n' +             
-              'echo \n' +             
+                'echo "************************************************************************************"\n' +             
               "echo named-checkzone failed due to errors in the file, please fix and run the job again\n" +
-              'echo \n' +             
-              'echo \n' +             
-              'echo \n' +             
-              "exit 1\n" +
+                'echo "*************************************************************************************"\n' +
+                 "exit 1\n" +
               "fi\n"
             }
           }
+
 
         project / 'properties' <<'com.coravy.hudson.plugins.github.GithubProjectProperty'(plugin:'github@1.11.3'){
               projectUrl 'https://github.com/Einavf/bind-dns.git'
@@ -50,12 +47,47 @@ freeStyleJob("Bind-DNS-test1"){
           def publishers = project / 'publishers'
             publishers << {
 
-              'hudson.tasks.Mailer'{
-                  recipients "\${ghprbActualCommitAuthorEmail}"
-                  dontNotifyEveryUnstableBuild false
-                  sendToIndividuals false
+               'hudson.plugins.s3.S3BucketPublisher'(plugin:'s3@0.10.12'){
+                  'profileName' bind-dns-to-s3
+                    'entries' {
+                        'hudson.plugins.s3.Entry' {
+                           'bucket' ("fuji-dns")
+                            'sourceFile' ("example.com.txt")
+                            'excludedFile'
+                            'storageClass' STANDARD
+                            'selectedRegion' ("us-west-2")
+                            'noUploadOnFailure' true
+                            'uploadFromSlave' false
+                            'managedArtifacts' true
+                            'useServerSideEncryption' false
+                            'flatten' false
+                            'gzipFiles' false
+                            'showDirectlyInBrowser' false
+                            'keepForever' false
+                                                     
+                        }
+                      'dontWaitForConcurrentBuildCompletion' false
+                        'consoleLogLevel' {
+                          'name' INFO
+                            'value' 800
+                            'resourceBundleName' ("sun.util.logging.resources.logging")
+                          }
+                      'pluginFailureResultConstraint' {
+                        'name' FAILURE
+                        'ordinal' 2 
+                        'color' RED
+                        'completeBuild' true
+                        }
+                      'userMetadata'
+                        
+                       
+                   }
+                        
+                    }
+                 
+              
               }
-          }
+        
     }
 
     wrappers {
